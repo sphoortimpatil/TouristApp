497bff816484157023e07d68b575183f775aa6bf
Sphoorti M Patil <sphoortimpatil@gmail.com>
2024-10-16 19:59:24 +0530

    created secrets for api keys

--- Tourist App/Constants.swift
+++ Tourist App/Constants.swift
@@ -10,9 +10,9 @@ import Foundation
 struct Constants  {
     struct GooglePlacesApi {
         static let apiKey  = "x-rapidapi-key"
-        static let apiValue = "d1b1896469msh85c2ec2bc978d3ep1ada6bjsn86a5d2953bf6"
+        static let apiValue = Bundle.main.infoDictionary?["GOOGLE_PLACES_API_KEY_VALUE"] as? String ?? ""
         static let apiHostKey = "x-rapidapi-host"
-        static let apiHostValue = "google-map-places.p.rapidapi.com"
+        static let apiHostValue = Bundle.main.infoDictionary?["GOOGLE_PLACES_API_HOST_VALUE"] as? String ?? ""
         static let autocompleteSearchApiUrl = "https://google-map-places.p.rapidapi.com/maps/api/place/queryautocomplete/json?radius=1000&input="
         static let placeDescriptionApi = "https://google-map-places.p.rapidapi.com/maps/api/place/details/json?fields=name,geometry&place_id="
         static let nearbyPlacesAPi = "https://google-map-places.p.rapidapi.com/maps/api/place/nearbysearch/json?radius=5000&type=tourist_attraction&location="
@@ -20,16 +20,16 @@ struct Constants  {
     
     struct MapsDataApi {
         static let apiKey  = "x-rapidapi-key"
-        static let apiValue = "d1b1896469msh85c2ec2bc978d3ep1ada6bjsn86a5d2953bf6"
+        static let apiValue = Bundle.main.infoDictionary?["MAPS_DATA_API_KEY_VALUE"] as? String ?? ""
         static let apiHostKey = "x-rapidapi-host"
-        static let apiHostValue = "maps-data.p.rapidapi.com"
+        static let apiHostValue = Bundle.main.infoDictionary?["MAPS_DATA_API_HOST_VALUE"] as? String ?? ""
         static let placeInfoApi = "https://maps-data.p.rapidapi.com/place.php?place_id="
         static let nearbyPlacesDataApi = "https://maps-data.p.rapidapi.com/nearby.php?query=Tourist attraction&limit=30&"
     }
     
     struct PexelApi {
         static let header = "Authorization"
-        static let value = "JAE9pM4hfeoI3B6cXTfVFtpSkvFq3tDNdA9g0EURAvEpbc1QUG6XHR7R"
+        static let value = Bundle.main.infoDictionary?["PEXEL_API_KEY_VALUE"] as? String ?? ""
         static let url = "https://api.pexels.com/v1/search?per_page=20&query="
     }
 }
